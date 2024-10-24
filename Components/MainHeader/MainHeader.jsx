7a670c3412ea8d6b89bdeb1af04e8b2a71759f58
yasubdemissie <yasdam777@gmail.com>
2024-10-24 11:38:27 +0300

    second commit

--- Components/MainHeader/MainHeader.jsx
+++ Components/MainHeader/MainHeader.jsx
@@ -1,13 +1,17 @@
 import Link from "next/link"
 import LogoImg from "@/assets/logo.png";
+import styles from "./main-header.module.css";
+import Image from "next/image";
 
 function MainHeader() {
+
+
     return (
-        <header>
-            <Link href={'/'}>
-                <img src={LogoImg.src} alt="A food on a plate" />
+        <header className={styles.header}>
+            <Link className={styles.logo} href={'/'}>
+                <Image src={LogoImg} alt="A food on a plate" priority />
             </Link>
-            <nav>
+            <nav className={styles.nav}>
                 <ul>
                     <li>
                         <Link href={'/meals'}>Meals</Link>
--- app/page.js
+++ app/page.js
@@ -1,16 +1,53 @@
 import Link from "next/link";
 
+import styles from "./Page.module.css";
+import Slideshow from "@/Components/SlideShow/Slideshow";
+
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-      <div>
-        <Link href="/meals">Meals</Link>
-        <Link href="/meals/share">Meals Share</Link>
-        <Link href="/community">Community</Link>
-      </div>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.slideshow}>
+          <Slideshow />
+        </div>
+        <div>
+          <div className={styles.hero}>
+            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <p>Taste & share food from all over the world.</p>
+          </div>
+          <div className={styles.cta}>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className={styles.section}>
+          <h2>How it works</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+
+        <section className={styles.section}>
+          <h2>Why NextLevel Food?</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+      </main>
+    </>
   );
 }
